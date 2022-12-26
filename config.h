d4acaf9fa977aee80c16a38b2d2ac60c84e343b0
Chirikumbrah <basslo@protonmail.com>
2022-12-26 20:20:18 +0300

    added mpv and ffplay window rules

--- config.h
+++ config.h
@@ -192,6 +192,8 @@ static const Rule rules[] = {
 
   // floating windows
 	RULE(.class = "feh",       .isfloating = 1)
+	RULE(.class = "mpv",       .isfloating = 1)
+	RULE(.class = "ffplay",    .isfloating = 1)
 
   // FIRST
 	RULE(.class = "Chromium",                    .tags = 1)
