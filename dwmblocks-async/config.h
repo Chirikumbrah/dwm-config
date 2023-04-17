bddc97d3d777bdcd5e5642390fcc390d9603fa07
Chirikumbrah <basslo@protonmail.com>
2023-04-17 12:28:31 +0300

    disabled volume widget

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -5,7 +5,7 @@
 
 const Block blocks[] = {
 	// BLOCK(scriptDir "xbps-updates.sh",      333,     0),
-	BLOCK(SCRIPTS "vol-dwm.sh",           10,      1),
+	// BLOCK(SCRIPTS "vol-dwm.sh",           10,      1),
 	BLOCK(SCRIPTS "mic-dwm.sh",           10,      2),
 	BLOCK(SCRIPTS "backlight.sh",         0,       3),
 	BLOCK(SCRIPTS "cputemp.sh",           5,       0),
